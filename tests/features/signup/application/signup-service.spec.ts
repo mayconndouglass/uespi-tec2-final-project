33c35f9200cb12dc7e313988b49af8a0e1829f5b
mayconndouglass <mayconn.douglass1414@gmail.com>
2025-01-12 02:02:25 -0300

    test: ensures  Should returns a UserAccount on successful signupService.execute()

--- tests/features/signup/application/signup-service.spec.ts
+++ tests/features/signup/application/signup-service.spec.ts
@@ -1,4 +1,5 @@
 import { SignupService } from '../../../../src/features/signup/application/services/signup-service'
+import { UserAccount } from '../../../../src/features/signup/domain/user-account'
 
 describe('SignupService', () => {
   it('should call SignupService.execute() with correct parameters', () => {
@@ -17,5 +18,31 @@ describe('SignupService', () => {
 
     expect(executeSpy).toHaveBeenCalledTimes(1)
     expect(executeSpy).toHaveBeenCalledWith(user)
+  })
+
+  it('Should returns a UserAccount on successful signupService.execute()', async () => {
+    const sut = new SignupService()
+    const user = {
+      name: 'John Doe',
+      email: 'john.doe@example.com',
+      cpf: '12345678900',
+      isPassenger: true,
+      isDriver: false,
+      password: 'password'
+    }
+    
+    const responseData: UserAccount = await sut.execute(user)
+
+    expect(responseData).toBeInstanceOf(UserAccount)
+    expect(responseData.id).toBeDefined()
+    expect(typeof responseData.id).toBe('string')
+    expect(responseData).toEqual({
+      id: responseData.id,
+      name: 'John Doe',
+      email: 'john.doe@example.com',
+      cpf: '12345678900',
+      isPassenger: true,
+      isDriver: false,
+    })
   })
 })
