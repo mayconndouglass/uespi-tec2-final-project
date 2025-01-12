fce61e1583ef02a6072d672b204b9fd6993000b3
mayconndouglass <mayconn.douglass1414@gmail.com>
2025-01-12 03:02:54 -0300

    feat: ensures should returns a UserAccount on successful signupService.execute()

--- src/features/signup/application/services/signup-service.ts
+++ src/features/signup/application/services/signup-service.ts
@@ -1,8 +1,22 @@
-export class SignupService {
-  execute(input: SignupService.input): void { }
+import { Signup } from "../../domain/contratcs/signup"
+import { UserAccount } from "../../domain/user-account"
+
+export class SignupService implements Signup {
+  async execute(input: SignupService.input): Promise<UserAccount> {
+    const user = new UserAccount(
+      input.name,
+      input.email,
+      input.cpf,
+      input.isPassenger,
+      input.isDriver,
+      input.carPlate,
+    )
+
+    return user
+  }
 }
 
-namespace SignupService {
+export namespace SignupService {
   export type input = {
     name: string
     email: string
@@ -13,5 +27,5 @@ namespace SignupService {
     carPlate?: string
   }
   
-  export type output = void
+  export type output = Promise<UserAccount>
 }
