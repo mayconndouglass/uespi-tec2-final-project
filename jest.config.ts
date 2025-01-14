c67571ebb4c5c26b491cb402aa3efe9b5babce5e
mayconndouglass <mayconn.douglass1414@gmail.com>
2025-01-13 22:36:58 -0300

    chore: setting up aliases in the project

--- jest.config.ts
+++ jest.config.ts
@@ -6,8 +6,8 @@ const config: Config = {
   coverageDirectory: "./coverage",
   coverageProvider: "babel",
   moduleNameMapper: {
-    "@/tests/(.+)": "<rootDir>/tests/$1",
-    "@/(.+)": "<rootDir>/src/$1",
+    "@/(.*)": "<rootDir>/src/$1",       // Alvo direto para arquivos em `src`
+    "@/tests/(.*)": "<rootDir>/tests/$1" // Alvo direto para arquivos em `tests`
   },
   roots: ["<rootDir>/src", "<rootDir>/tests"],
   testEnvironment: "jest-environment-node",
